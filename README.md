364d1a92195c49f7418ed283575e10cf955c47e7
[brycen] <[brycenbartolome@gmail.com]>
2020-08-28 10:33:47 -0700

    add topping function add size function

--- README.md
+++ README.md
@@ -19,7 +19,15 @@ _Version 1, Date_
 ## _Specs_
 1. User inputs name, toppings, size, and program returns an object:
 input: (brycen, peperoni, large);
-output:  
+output: pizza {}
+
+2. User chooses size of pizza and it affects base cost:
+input: medium
+output: basecost = 10
+
+3. user adds a topping it add 2 to topping cost:
+input: sausage
+output: topping cost = 2
 
 ## _Repository_
 * [gh-pages](https://brycengit.github.io//)
--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,49 @@ function Orders() {
   this.totalOrders = 0;
 }
 
-function Pizza(orderName, toppings, size) {
-  this.name = orderName;
-  this.toppings = toppings;
-  this.size = size;
-  this.basePrice = 10;
-}
-
 Orders.prototype.addOrder = function(pizzaParameter) {
   this.orders.push(pizzaParameter);
   this.totalOrders += 1;
 }
 
+function Pizza(orderName, toppings, size) {
+  this.name = orderName;
+  this.toppings = toppings;
+  this.size = size;
+  this.basePrice = 0;
+  this.toppingCost = 0;
+  this.pizzaTotal = 0;
+}
+
+function pizzaSize(pizzaParameter) {
+  if (pizzaParameter.size === "large") {
+    alert("large0!");
+    pizzaParameter.basePrice = 12;
+  } else if (pizzaParameter.size === "medium") {
+    alert('medium');
+    pizzaParameter.basePrice = 10;
+  } else {
+    alert('small');
+    pizzaParameter.basePrice = 8;
+  }
+}
+
+Pizza.prototype.addToppings = function() {
+  this.toppingCost +=2;
+}
+
+Pizza.prototype.addPizzaTotal = function() {
+  this.pizzaTotal = this.basePrice + this.toppingCost;
+}
+
 let pizzaOrders = new Orders();
 console.log(pizzaOrders);
-let pizza = new Pizza("brycen", "pepperoni", "large")
+let pizza = new Pizza("brycen", ["pepperoni", 'peppers', 'sausage'], "medium")
 console.log(pizza);
 pizzaOrders.addOrder(pizza);
 console.log(pizzaOrders);
+console.log(pizza);
+pizza.addPizzaTotal();
+console.log(pizza);
+pizzaSize(pizza);
+console.log(pizza);
